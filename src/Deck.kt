85c269f306ac3c72f65298e3d4752a1cfe739a51
ChanJoo <sksckswn@naver.com>
2018-03-31 01:55:50 +0900

    Added to functions for Deck class

--- src/Deck.kt
+++ src/Deck.kt
@@ -1,6 +1,16 @@
+import java.util.*
+
 class Deck {
 
     val cards = Array(52, { Card(it % 3, getSuit(it)) })
+    var cardsInDeck: MutableList<Card> = cards.toMutableList()
+
+    fun drawCard(): Card = cardsInDeck.removeAt(0) // take card indexed 0
+
+    fun reset() { // reset and shuffle the deck
+        cardsInDeck = cards.toMutableList()
+        Collections.shuffle(cardsInDeck)
+    }
 
     private fun getSuit(i: Int) = when(i / 13) { // == switch
             0 -> "Clubs"
